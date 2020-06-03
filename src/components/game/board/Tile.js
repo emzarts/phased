15dbedf17e106317d92eb35c40a5ed823d670d3c
Emily Zimmerman <17zimmermane@gmail.com>
2020-06-02 21:35:05 -0500

    made tile into a button

--- src/components/game/board/Tile.js
+++ src/components/game/board/Tile.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default ({row, col}) => {
     return (
         <div className="Board-Tile">
-            <div className="Board-Tile-Content">
-            </div>    
+            <button className="Board-Tile-Content">
+            </button>    
         </div>
     );
 };
