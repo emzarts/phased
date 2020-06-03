f6097a0aebbf20d2178019e6a1f0f055c7951d34
Emily Zimmerman <17zimmermane@gmail.com>
2020-06-03 18:22:15 -0500

    moved logic into board

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -3,15 +3,6 @@ import PropTypes from 'prop-types';
 import Board from './board/Board';
 import './Game.css';
 
-const TileColor = {
-    EMPTY: 0,
-    RED: 1,
-    YELLOW: 2,
-    GREEN: 3,
-    BLUE: 4,
-    PURPLE: 5 
- };
-
 export default class Game extends React.Component {
     constructor(props) {
         super(props);
@@ -19,46 +10,10 @@ export default class Game extends React.Component {
             inProgress: true,
             currentScore: 0,
             interval_sec: .5,
-            boardState: Array(this.props.rows).fill().map(() => Array(this.props.cols).fill(TileColor.EMPTY)),
             highScore: this.props.highSchore
         }
         this.handleTilePress = this.handleTilePress.bind(this);
-        this.clearBoard = this.clearBoard.bind(this);
-        this.setTile = this.setTile.bind(this);
     }
-
-    clearBoard() {
-        this.setState(
-            {
-                boardState: Array(this.props.rows).fill().map(() => Array(this.props.cols).fill(TileColor.EMPTY)),
-            }
-        );
-    }
-
-    setTile(row, col, color) {
-        let boardState = this.state.boardState;
-        boardState[row][col] = color;
-        this.setState(
-            {
-                boardState: boardState
-            }
-        );
-    }
-
-    componentDidMount() {
-        const rows = this.props.rows;
-        const cols = this.props.cols;
-        setInterval(() => {
-            console.log('Interval triggered');
-            this.clearBoard();
-            let row = Math.floor(Math.random() * rows);
-            let col = Math.floor(Math.random() * cols);
-            console.log(row, col);
-            this.setTile(row, col, 1);
-        }, 1000 * this.state.interval_sec);
-
-
-    };
 
     handleTilePress(row, col) {
         console.log(row, col);
@@ -69,7 +24,7 @@ export default class Game extends React.Component {
         return (
             <div className="Game">
                 <div className="Game-Contents">
-                    <Board boardState={this.state.boardState} height={this.props.rows} width={this.props.cols} handleTilePress={this.handleTilePress} />
+                    <Board height={this.props.rows} width={this.props.cols} handleTilePress={this.handleTilePress} />
                 </div>
             </div>
         );
--- src/components/game/board/Board.js
+++ src/components/game/board/Board.js
@@ -1,26 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Board.css';
-import Tile from './Tile';
+import Tile from './tile/Tile';
 
- const ColorTile = {
-     0: 'Empty',
-     1: 'Red',
-     2: 'Yellow',
-     3: 'Green', 
-     4: 'Blue',
-     5: 'Purple'
- }
+const ColorTile = {
+    0: 'Empty',
+    1: 'Red',
+    2: 'Yellow',
+    3: 'Green', 
+    4: 'Blue',
+    5: 'Purple'
+}
+
+const TileColor = {
+    EMPTY: 0,
+    RED: 1,
+    YELLOW: 2,
+    GREEN: 3,
+    BLUE: 4,
+    PURPLE: 5 
+};
 
 export default class Board extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            interval_sec: .5,
+            boardState: Array(this.props.height).fill().map(() => Array(this.props.width).fill(TileColor.EMPTY)),
+        }
+        this.clearBoard = this.clearBoard.bind(this);
+        this.setTile = this.setTile.bind(this);
         this.createBoardTiles = this.createBoardTiles.bind(this);
     };
 
+    clearBoard() {
+        this.setState(
+            {
+                boardState: Array(this.props.height).fill().map(() => Array(this.props.width).fill(TileColor.EMPTY)),
+            }
+        );
+    }
+
+    componentDidMount() {
+        const rows = this.props.height;
+        const cols = this.props.width;
+        setInterval(() => {
+            this.clearBoard();
+            let row = Math.floor(Math.random() * rows);
+            let col = Math.floor(Math.random() * cols);
+            this.setTile(row, col, 1);
+        }, 1000 * this.state.interval_sec);
+    };
+
+
+    setTile(row, col, color) {
+        let boardState = this.state.boardState;
+        boardState[row][col] = color;
+        this.setState(
+            {
+                boardState: boardState
+            }
+        );
+    }
+
     createBoardTiles = () => {
         let board = []
-        let boardState = this.props.boardState;
+        let boardState = this.state.boardState;
+        console.log(boardState);
         
         for (let rows = 0; rows < this.props.height; rows++) {
             let row = []
